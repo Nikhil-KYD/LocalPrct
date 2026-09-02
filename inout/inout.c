4d0f3c6916a03b8ffbc00096bfb6bcd041597115
Nikhil-git <voyagerelev@gmail.com>
2026-09-02 13:57:18 +0530

    updates

---
+++ inout/inout.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+int num = 120;
+char oki = 'a';
+
+
+printf("Octal is: %o\n", num);
+printf("Hexadecimal is: %x\n", num);
+printf("ASCII value is: %d", oki);
+
+
+
+
+}
--- sum.c
+++ sum.c
@@ -1,8 +1,8 @@
-#include<Stdio.h>
+#include <stdio.h>
 int main()
 {
     float a,b,c;
-    a= 5.0, b=6.0, c=8.0;
+    a= 5.0, b=6.0, c=2.2;
     float sum;
     sum=a+b+c;
     printf("The sum of a,b,c is %f\n",sum);
